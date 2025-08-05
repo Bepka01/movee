881a4edead378086381c17faf17f8e06647e4d2c
unknown <nichegolichnogo1717@gmail.com>
2025-08-06 00:35:25 +0300

    added_burger

--- sctipt.js
+++ sctipt.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".btn__dropdown");
 const dropdown = document.querySelector(".wrapper__add-servis");
+const btnBurger = document.querySelector(".header__burger");
+const burgerSlide = document.querySelector(".burger__slide");
+const closeBurger = document.querySelector(".close__burger");
 
 btn.addEventListener("click", function () {
   if (dropdown.classList.contains("close") === true) {
@@ -11,4 +14,16 @@ btn.addEventListener("click", function () {
   } else {
     btn.classList.add("rotate");
   }
+});
+
+btnBurger.addEventListener("click", function showCloseBurgerMenu() {
+  if (burgerSlide.classList.contains("disp") === true) {
+    burgerSlide.classList.remove("disp");
+  } else burgerSlide.classList.add("disp");
+});
+
+closeBurger.addEventListener("click", function () {
+  if (burgerSlide.classList.contains("disp") === true) {
+    burgerSlide.classList.remove("disp");
+  } else burgerSlide.classList.add("disp");
 });
