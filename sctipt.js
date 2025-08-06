d576983b27aa86b5a8a6da478944e6c1c357cff4
unknown <nichegolichnogo1717@gmail.com>
2025-08-06 20:32:21 +0300

    addedBurger

--- sctipt.js
+++ sctipt.js
@@ -3,27 +3,20 @@ const dropdown = document.querySelector(".wrapper__add-servis");
 const btnBurger = document.querySelector(".header__burger");
 const burgerSlide = document.querySelector(".burger__slide");
 const closeBurger = document.querySelector(".close__burger");
+const body = document.querySelector("body");
 
 btn.addEventListener("click", function () {
-  if (dropdown.classList.contains("close") === true) {
-    dropdown.classList.remove("close");
-  } else dropdown.classList.add("close");
+  dropdown.classList.toggle("close");
 
-  if (btn.classList.contains("rotate")) {
-    btn.classList.remove("rotate");
-  } else {
-    btn.classList.add("rotate");
-  }
+  btn.classList.toggle("rotate");
 });
 
-btnBurger.addEventListener("click", function showCloseBurgerMenu() {
-  if (burgerSlide.classList.contains("disp") === true) {
-    burgerSlide.classList.remove("disp");
-  } else burgerSlide.classList.add("disp");
+btnBurger.addEventListener("click", function showBurgerMenu() {
+  burgerSlide.classList.toggle("disp");
+  body.classList.toggle("lock");
 });
 
-closeBurger.addEventListener("click", function () {
-  if (burgerSlide.classList.contains("disp") === true) {
-    burgerSlide.classList.remove("disp");
-  } else burgerSlide.classList.add("disp");
+closeBurger.addEventListener("click", function closeBurgerMenu() {
+  burgerSlide.classList.toggle("disp");
+  body.classList.toggle("lock");
 });
