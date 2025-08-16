ae51402fd7143a695a7a1e65c3fd48e137e41d5e
unknown <nichegolichnogo1717@gmail.com>
2025-08-17 01:06:20 +0300

    added slider

--- sctipt.js
+++ sctipt.js
@@ -4,7 +4,7 @@ const btnBurger = document.querySelector(".header__burger");
 const burgerSlide = document.querySelector(".burger__slide");
 const closeBurger = document.querySelector(".close__burger");
 const body = document.body;
-let date = new Date("aug 8 2025 23:35:00");
+let date = new Date("aug 18 2025 8:00:00");
 
 btn.addEventListener("click", function () {
   dropdown.classList.toggle("close");
@@ -30,10 +30,62 @@ function counts() {
     document.querySelector(".timer").innerText = "Акция завершена";
     document.querySelector(".timer").style.fontSize = "40px";
   } else {
-    document.querySelector(".hours").innerText = hours;
-    document.querySelector(".minutes").innerText = minutes;
-    document.querySelector(".seconds").innerText = seconds;
+    document.querySelector(".hours").innerText =
+      hours < 10 ? "0" + hours : hours;
+    document.querySelector(".minutes").innerText =
+      minutes < 10 ? "0" + minutes : minutes;
+    document.querySelector(".seconds").innerText =
+      seconds < 10 ? "0" + seconds : seconds;
   }
 }
 
 setInterval(counts, 1000);
+
+document.addEventListener("DOMContentLoaded", function () {
+  let position = 0;
+  const slidesToShow = 1;
+  const slidesToScroll = 1;
+  const sliderContainer = document.querySelector(".slider__container");
+  const sliderTrack = document.querySelector(".slider__track");
+  const sliderItems = document.querySelectorAll(".slider__item");
+  const btnNext = document.querySelector(".btn__next");
+  const btnPrev = document.querySelector(".btn__prev");
+  const sliderCount = sliderItems.length;
+  const itemWidth = sliderContainer.clientWidth / slidesToShow;
+  const movePosition = slidesToScroll * itemWidth;
+
+  sliderItems.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+  });
+
+  btnNext.addEventListener("click", function () {
+    const itemsLeft =
+      sliderCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+
+    position -=
+      itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+  });
+
+  btnPrev.addEventListener("click", function () {
+    const itemsLeft = Math.abs(position) / itemWidth;
+
+    position +=
+      itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+
+    setPosition();
+    checkBtns();
+  });
+
+  const setPosition = () => {
+    sliderTrack.style.transform = `translateX(${position}px)`;
+  };
+
+  const checkBtns = () => {
+    btnPrev.disabled = position === 0;
+    btnNext.disabled = position <= -(sliderCount - slidesToShow) * itemWidth;
+  };
+  checkBtns();
+});
